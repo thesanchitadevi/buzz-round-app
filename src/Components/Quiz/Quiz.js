38e8b8376f3f38567b7020d858e0b60cee2b2391
sanchitadevi <thesanchitadevi@gmail.com>
2022-10-11 02:46:08 +0600

    redirects file added

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -7,10 +7,11 @@ const Quiz = ({ quiz }) => {
 
     return (
         <div>           
-            <div className="card w-80 bg-base-100 shadow-xl image-full mx-auto my-10">
+            <div className="card w-80 bg-gray-200 shadow-xl image-full mx-auto my-10">
                 <figure><img src={logo} alt="...." /></figure>
-                <div className="card-body">
+                <div className="card-body justify-center items-center">
                     <h2 className="text-center text-2xl font-bold">{name}</h2>
+                    <h2 className="text-center text-xl font-bold">Total Quiz : {total}</h2>
                     <div className="card-actions justify-center">
                         <button className="px-14 py-2 rounded bg-emerald-600">Take Quiz</button>
                     </div>
