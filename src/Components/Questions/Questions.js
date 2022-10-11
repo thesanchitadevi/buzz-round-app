5f11f0e8bd93959edb6d75555008f811bfc86633
sanchitadevi <thesanchitadevi@gmail.com>
2022-10-11 18:53:28 +0600

    toast added

--- src/Components/Questions/Questions.js
+++ src/Components/Questions/Questions.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
-const Questions = ({ questions,question }) => {
-    
+const Questions = ({ question, handleButton }) => {
+
     return (
-        <div className='bg-slate-400 text-center text-xl font-medium m-5 rounded-xl py-6'>
-            <h1>Quiz : {question.question} </h1>
-            <section >
-                {question.options.map((e, i) => (i + 1 + "." + e)).join(' ')}
+        <div>
+            <ToastContainer></ToastContainer>
+            <section>
+                <div className='bg-slate-100 shadow-lg mx-auto text-center w-1/2 text-xl font-medium m-5 rounded-xl p-6'>
+                    <h1>Quiz : {question.question.slice(3, -4)} </h1>
+                    {
+                        question.options.map(option =>
+                            <div onClick={() => handleButton(option, question.correctAnswer)} className='cursor-pointer border border-emerald-300  text-center hover:text-emerald-600 p-2 my-4 rounded '>
+                                {option}
+                            </div>)
+                    }
+                </div>
             </section>
         </div>
     );
--- src/Components/QuizDetails/QuizDetails.js
+++ src/Components/QuizDetails/QuizDetails.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Questions from '../Questions/Questions';
+import { toast } from 'react-toastify';
 
 const QuizDetails = () => {
     const quiz = useLoaderData();
+    const [rightAnswer, setRightAnswer] = useState([]);
     // console.log(quiz);
     const quizQues = quiz.data;
     const { name, questions } = quizQues;
-    // console.log(questions);
+    console.log(questions);
+
+    const rightMessage = () => {
+        toast.success('The Answer is Right !', {
+            position: toast.POSITION.TOP_RIGHT
+        });
+    };
+    const wrongMessage = () => {
+        toast.error('Alas ! You choose a Wrong Answer.', {
+            position: toast.POSITION.TOP_RIGHT
+        });
+    };
+
+    const handleButton = (chooseAnswer, correctAnswer) => {
+        if (chooseAnswer === correctAnswer) {
+            rightMessage()
+            if (!(rightAnswer.includes(correctAnswer))) {
+                let rightAnsArr = [...rightAnswer, correctAnswer]
+                setRightAnswer(rightAnsArr)
+            }
+        }
+        else {
+            wrongMessage()
+        }
+    }
+
     return (
         <div>
             <h1>Here quiz ques : { name}</h1>
@@ -15,7 +42,9 @@ const QuizDetails = () => {
             {
                 questions.map(question => <Questions
                     key={question.id}
-                    question={question}></Questions>)
+                    question={question}
+                    handleButton={handleButton}
+                ></Questions>)
             }
         </div>
     );
